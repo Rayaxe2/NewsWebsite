ce84902bd9a3aa1453c1966cbaaeeed82aeec41f
nutt3r <48687616+nutt3r@users.noreply.github.com>
2020-11-25 01:09:46 +0000

    Creates AppUser model

--- NewsApp/models.py
+++ NewsApp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class AppUser(User):
+    # An AppUser inherits from User but adds Date of Birth to a similar naming convention.
+
+    date_of_birth = models.DateField()
+
+    @property
+    def fullName(self):
+        return self.first_name + ' ' + self.last_name
+    
+    def __str__(self):
+        return self.fullName

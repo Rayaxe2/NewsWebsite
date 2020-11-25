6867e43d192dcc587eb8d5833661f438ddb9e06b
nutt3r <48687616+nutt3r@users.noreply.github.com>
2020-11-25 12:13:53 +0000

    Adds basic models representing categories and articles

--- NewsApp/admin.py
+++ NewsApp/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 from NewsApp import models
 
 admin.site.register(models.AppUser)
+
+admin.site.register(models.NewsArticle)
+
+admin.site.register(models.NewsCategory)
--- NewsApp/models.py
+++ NewsApp/models.py
@@ -13,3 +13,19 @@ class AppUser(User):
     
     def __str__(self):
         return self.fullName
+
+class NewsArticle(models.Model):
+    headline = models.CharField(max_length=200)
+    author = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    pub_date = models.DateField(auto_now_add=True)
+    story = models.TextField()
+
+    def __str__(self):
+        return self.headline
+
+class NewsCategory(models.Model):
+    title = models.CharField(max_length=100)
+    articles = models.ManyToManyField(NewsArticle, blank=True)
+
+    def __str__(self):
+        return self.title

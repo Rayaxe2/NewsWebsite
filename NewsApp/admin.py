667bb1b2948b5742c6c90c62094b44cdb1459183
nutt3r <48687616+nutt3r@users.noreply.github.com>
2020-11-27 03:11:57 +0000

    Resolves issue with AppUser/Hashing passwords

--- NewsApp/admin.py
+++ NewsApp/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from NewsApp import models
 
-admin.site.register(models.AppUser)
+class AppUserAdmin(UserAdmin):
+    # Add fields to AppUser creation form on the admin site
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'is_superuser', 'is_staff', 'is_active',)}),
+    )
+    filter_horizontal = ()
 
+# Register models for admin site.
+admin.site.register(models.AppUser, AppUserAdmin)
 admin.site.register(models.NewsArticle)
-
 admin.site.register(models.NewsCategory)
